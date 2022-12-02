3cf6eed42e195fcd7c9f0389d4aec40583acd3de
MTC <matheustcavalcanti@hotmail.com>
2022-12-01 21:03:52 -0300

    Implementando Método ValidarCnpj

--- Classes/PessoaJuridica.cs
+++ Classes/PessoaJuridica.cs
@@ -1,18 +1,47 @@
 using CadastroPessoaFF12.Interfaces;
+using System.Text.RegularExpressions;
 namespace CadastroPessoaFF12.Classes
 {
-    public class PessoaJuridica : Pessoa, IPessoaJuridica 
+    public class PessoaJuridica : Pessoa, IPessoaJuridica
     {
-        public string ?Cnpj {get; set; }
-         public string ?RazaoSocial {get; set; }
-        public bool ValidarCnpj (bool ValidarCnpj) // Método que foi herdado da "INTERFACE" 
-        { 
-         throw new NotImplementedException(); //Não retorna nada
+        public string? Cnpj { get; set; }
+        public string? NomeFantasia { get; set; }
+        public string? RazaoSocial { get; set; }
+        public bool ValidarCnpj(string cnpj) // Método que foi herdado da "INTERFACE" 
+        {
+            //"02.023.134/0001-31"; 18 
+            // "02023134000131"; 14
+            // Veriifcar se tem 001
+            if (Regex.IsMatch(cnpj, @"^\d{2}.\d{3}.\d{3}/\d{4}-\d{2}|(\d{14})$"))
+            {
+
+                if (cnpj.Length == 18)
+                {// se for igual a 18 caracteres 
+
+                    if (cnpj.Substring(11, 4) == "0001") // A PARTIR DO CARACTE 11 NA NUMERAÇÃO, PEGAR OS PRÓXIMOS 4 DIGITOS
+                    {// Verificar se tem 0001
+                        return true;
+                    }
+
+                }
+                else if (cnpj.Length == 14)
+                {// Verificar se for igual a 14 caracteres.
+                    if (cnpj.Substring(8, 4) == "0001")// A PARTIR DO CARACTE 8  NA NUMERAÇÃO, PEGAR OS PRÓXIMOS 4 DIGITOS
+                    {// Verificar se tem 0001
+                        return true;
+                    }
+
+                }
+
+                return true;
+            }
+
+            return false;
         }
 
-        //Conceito de "OVERRIDE" Vem do fato de o pagamento ser de maneira difereten , Ex: IRpf de 3% para PF E 10% PJ
+        //Conceito de "OVERRIDE" Vem do fato de o pagamento(método) ser de maneira diferete , Ex: IRpf de 3% para PF E 10% PJ
         // Aqui podemos aplciar o conceito de polimorfismo
-        public override float PagarImposto(float rendiemento) 
+        public override float PagarImposto(float rendiemento)
         {
             throw new NotImplementedException();
         }
--- Program.cs
+++ Program.cs
@@ -1,14 +1,17 @@
 ﻿using CadastroPessoaFF12.Classes;
-
-
-
-
+using System.Text.RegularExpressions; //Import da classe Regex
+Console.WriteLine();
+Console.WriteLine(@"********* Pessoa Física**********");
+//***********Cadastro de Pessoa Fisica************\\
+Console.WriteLine();
 Endereco endfpf1 = new Endereco();// Instanciando Classe endereço New Endereço()
 endfpf1.Logradouro = "Endereço 1";
 endfpf1.Numero = 47;
 endfpf1.Comercial = false;
 
 PessoaFisica pf1 = new PessoaFisica(); //Instancia a classe pessoa Fisica e cria um objeto 
+PessoaFisica MetodosPf =  new PessoaFisica();// Instanciando um obejto para chamar os métodos.
+
 pf1.Nome ="Matheus"; //declara o valor do objeto 
 pf1.Cpf = "1111.1111.111-21";//declara o valor do objeto(Atributo )
 pf1.DataNascimento = new DateTime(2000,05,14);
@@ -26,7 +29,78 @@ Console.WriteLine($"Número: {pf1.Endereco.Numero}");// objetos e seus valores
 Console.WriteLine($"Endereço comercial ? {pf1.Endereco.Comercial}");// objetos e seus valores
 Console.WriteLine($"Maior de Idade? {pf1.ValidarDataNascimento(pf1.DataNascimento)}");// Pega a data de nascimento Usa o método para calcular e retorna se é maior de idade.
 //Console.WriteLine($"Maior de Idade? {pf1.ValidarDataNascimento(14/05/2000)}") // Do método String de ValidarDataDeNascimento
-//DateTime data = new DateTime(2000, 5 , 14);
+DateTime data = new DateTime(2000, 5 , 14);
 
 //Console.WriteLine( data.ValidarDataNascimento(data));
+
+Console.WriteLine();
+Console.WriteLine();
+Console.WriteLine(@"********* Pessoa Júridica**********");
+
+//************Cadastro de pessoa Júridica**********\\ 
+
+
+
+/// Instanciando classse (que é uma composição) endereço e colocando valor nas variaveis. 
+Endereco endPj = new Endereco(); /// Instanciando a classe Endereço//
+endPj.Logradouro = "Rua albertino";
+endPj.Numero = 47;
+endPj.Comercial= true;
+
+// Instanciano classe PessoaJuridica e colocando valor em sua variaveis.
+PessoaJuridica novaPj = new PessoaJuridica();
+PessoaJuridica MetodosPj = new PessoaJuridica(); // Instanciando um obejto para chamar os métodos.
+
+novaPj.Nome = "Matheus Cavalcanti";
+novaPj.Endereco = endPj;
+novaPj.Rendimento = 1000;
+novaPj.Cnpj = "02.023.134/0001-31";
+// novaPj.Cnpj = "020231340001331";
+novaPj.RazaoSocial = "Matheus Soluções LTDA";
+novaPj.NomeFantasia = "MTCS";
+
+//*****Exemplo De caixa de pergunta(INPUT)****\\
+// Console.WriteLine("Qual o rendimento?"); // Pergunta QUAL É O RENDIMENTO
+// novaPj.Rendimento = float.Parse(Console.ReadLine()); Pega o que o usuario digitar, O  "float.parse" Pega o tipo de dado e transforma em caracteres.
+
+//*******Exibir dados*******\\
+
+// / Para aceitar Quebra de linha e Caracteres especias devemos colocar "@$"
+Console.WriteLine(@$" 
+Empresa : {novaPj.RazaoSocial}
+Nome Fantasia : {novaPj.NomeFantasia}
+Representante : {novaPj.Nome}
+CNPJ é Válido? : {MetodosPj.ValidarCnpj(novaPj.Cnpj)} 
+Redimento Anual  : {novaPj.Rendimento}
+Endereço : {novaPj.Endereco.Logradouro}
+Número : {novaPj.Endereco.Numero}
+É comercial ? {novaPj.Endereco.Comercial}
+");
+///
+/// /CNPJ é Válido? : {novaPj.ValidarCnpj(novaPj.Cnpj)}  : Verifica se o cnpj é valido, Armazendo como valor Cnpj.
+
+
+
+
+
+
+
+
+
+
+
+//***************** Estudos De caso**************\\
+///Sub-String\\
+
+// string nome ="Matheuswww";
+
+// Console.WriteLine(nome.Substring(3));// Pega os dados a partir do caracter 3;
+// Console.WriteLine(nome.Substring(03, 04)); /// Começa do caracter 03 e puxa mais 4 caracteres contando a partir dp 04.
+
+////Expressões Regulares\\\\
+
+// String  Data = "02/02/19900";
+// bool  dataValue = Regex.IsMatch(Data, @"^\d{2}/\d{2}/\d{4}$" ); //O método Regex recebe a string e identifica um padrão. Não pode ter espaço no começo. E inicia com "^" termina com "$'
+
+// Console.WriteLine(dataValue);
 
